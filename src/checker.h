17f2d313818d024fe92d1fb4a2aee2b995772d86
equation314 <equation618@gmail.com>
2016-06-24 01:35:28 +0800

    Move class Solution to box.h; Declare class TTree's methods

--- src/checker.h
+++ src/checker.h
@@ -1,13 +1,13 @@
 #ifndef CHECKER_H
 #define CHECKER_H
 
-#include "solution.h"
+#include "box.h"
 
 /// 方案验证
 class Checker
 {
 public:
-    Checker(const Solution* sol = NULL);
+    Checker(const Solution& sol);
 };
 
 #endif // CHECKER_H
--- src/ttree.h
+++ src/ttree.h
@@ -3,6 +3,8 @@
 
 #include "box.h"
 
+#include <vector>
+
 class TTreeNode
 {
 public:
@@ -15,6 +17,35 @@ class TTree
 public:
     TTree(const BoxList &boxes);
     ~TTree();
+    
+    /// Delete the box p
+    void Delete(int p);
+    
+    /// Insert the box p as box q's child
+    void InsertAsChild(int p, int q);
+    
+    /// Insert the box p to box q's place and let box q be the k-th chlid of box p
+    void InsertToReplace(int p, int q, int k);
+  
+      /// Swap 2 boxed at T-tree
+    void Swap(int p, int q);
+    
+    /// Rotation box p around the axis at direction dir
+    void Rotate(int p, int dir);
+    
+    /// Get the minimum bounding box of the placement according to T-tree
+    int GetVolume();
+    
+    /// Get the optimal solution
+    Solution GetSolution();
+    
+private:
+    TTreeNode* root;
+    std::vector<TTreeNode*> nodes;
+    PlacedBoxList boxes;
+    
+    /// Get T-tree node by box id
+    TTreeNode* getNodeById(int id);
 };
 
 #endif // T_TREE_H
