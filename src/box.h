cbd6ba53f3b91751bbb7516dee03953d36d8f07a
equation314 <equation618@gmail.com>
2016-06-24 18:12:28 +0800

    Demo && implement some functions

--- src/box.h
+++ src/box.h
@@ -6,8 +6,13 @@
 class Box
 {
 public:
+    Box() : l(0), w(0), h(0) {}
+    Box(int l, int w, int h) : l(l), w(w), h(h) {}
+
     /// 长，宽，高
     int l, w, h;
+
+    int Volume() { return l * w * h; }
 };
 typedef std::vector<Box> BoxList;
 
@@ -15,8 +20,15 @@ typedef std::vector<Box> BoxList;
 class PlacedBox: public Box
 {
 public:
-    /// 空间坐标
+    PlacedBox() : Box(), x(0), y(0), z(0), x2(0), y2(0), z2(0) {}
+    PlacedBox(int x, int y, int z, int l, int w, int h)
+        : Box(l, w, h), x(x), y(y), z(z), x2(x + l), y2(y + w), z2(z + h) {}
+
+    /// 左前下角坐标
     int x, y, z;
+
+    /// 右后上角坐标
+    int x2, y2, z2;
 };
 typedef std::vector<PlacedBox> PlacedBoxList;
 
@@ -24,9 +36,41 @@ typedef std::vector<PlacedBox> PlacedBoxList;
 class Solution
 {
 public:
+    Solution() : vol(0), total_vol(0), bounding_box() {}
+
+    int Size() { return boxes.size(); }
+
+    void Add(PlacedBox& box) { boxes.push_back(box); }
+    void Add(int x, int y, int z, int l, int w, int h) { boxes.push_back(PlacedBox(x, y, z, l, w, h)); }
+
+    /// Get the bounding box after placement
+    Box GetBoundingBox();
+
+    /// Get total volume of boxes
+    int GetBoxesVolume();
+
+    /// Get volume of the bounding box
+    int GetBoundingBoxVolume() { return vol ? vol : GetBoundingBox().Volume(); }
+
+    /// Get the wasted volume, = GetBoundingBoxVolume() – GetBoxesVolume()
+    int GetWastedVolume() { return GetBoundingBoxVolume() - GetBoxesVolume(); }
+
+    /// Get wasted volume ratio, = getWastedVolume() / GetBoundingBoxVolume()
+    double GetWastedVolumeRatio()
+    {
+        GetBoundingBoxVolume();
+        return vol ? 1.0 * GetWastedVolume() / vol : 0;
+    }
+
+    /// length(X), width(Y) and height(Z)
+
+    PlacedBoxList::iterator BoxesBegin() { return boxes.begin(); }
+    PlacedBoxList::iterator BoxesEnd() { return boxes.end(); }
 
 private:
     PlacedBoxList boxes;
+    int vol, total_vol;
+    Box bounding_box;
 };
 
 #endif // BOX_H
--- src/placement3d.h
+++ src/placement3d.h
@@ -3,21 +3,30 @@
 
 #include "box.h"
 
-#include <cstdlib>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 class Placement3D
 {
 public:
     static BoxList LoadBoxesFromFile(const std::string& fileName);
+    static Solution LoadSolutionFromFile(const std::string& fileName);
     static bool SaveBoxesToFile(const std::string& fileName, const BoxList& boxes);
+    static bool SaveSolutionToFile(const std::string& fileName, const Solution& sol);
 
-public:
-    Placement3D(const BoxList &boxes);
+    Placement3D(const BoxList& boxes);
     ~Placement3D();
 
+    /// Solve the 3D Placement Problem use Simulated Annealing + T-tree
     void Solve();
+
+    /// Get a placement solution
+    Solution GetSolution()
+    {
+        Solve();
+        return sol;
+    }
 
 private:
     BoxList boxes;
