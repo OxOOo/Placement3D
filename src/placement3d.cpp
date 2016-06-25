022a8777451eff9d1b3d45b52852fb551ae1f099
equation314 <equation618@gmail.com>
2016-06-26 01:57:46 +0800

    Simulated Annealing

--- src/placement3d.cpp
+++ src/placement3d.cpp
@@ -70,5 +70,58 @@ bool Placement3D::SaveSolutionToFile(const string& fileName, const Solution& sol
 /// Simulated Annealing
 void Placement3D::solve()
 {
-    
+    const double INIT_TEMPERATURE = 1000;
+    const double TEMPERATURE_DOWN_FACTOR = 0.99;
+    const double VOLUME_FACTOR = 1;
+
+    TTree* tree = new BTTree(boxes);
+    double T = INIT_TEMPERATURE;
+	double value = tree->GetSolution().GetBoundingBoxVolume() * VOLUME_FACTOR;
+
+	for (;T > 0.01; T *= TEMPERATURE_DOWN_FACTOR)
+    {
+		for (int k = 0; k < 100; k++)
+        {
+			// Get next status
+			TTree* newTree = tree->Clone();
+            int oper = Random::nextInt(3), p, q, dir;
+            switch (oper)
+            {
+            case 0: // Move p
+                p = Random::nextInt(n);
+                newTree->Move(p);
+                break;
+            case 1: // Swap p q
+                p = Random::nextInt(n);
+                q = Random::nextInt(n);
+                newTree->Swap(p, q);
+                break;
+            case 2: // Rotate p dir
+                p = Random::nextInt(n);
+                dir = Random::nextInt(5);
+                newTree->Rotate(p, dir);
+                break;
+            }
+
+            // Get next status value
+            Solution newSol = newTree->GetSolution();
+            int newValue = newSol.GetBoundingBoxVolume() * VOLUME_FACTOR;
+			double d = newValue - value;
+
+			// Accept
+			if (d < 0 || Random::nextDouble() < exp(- d / T))
+            {
+                sol = newSol;
+                value = newValue;
+                delete tree;
+                tree = newTree;
+			}
+		}
+
+        tree->Print();
+        cout << "temperature: " << T << " \t\t"
+             << "value: "       << value << " \t\t"
+             << "volume: "      << value << endl;
+	}
+    delete tree;
 }
