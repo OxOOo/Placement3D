ac2783883538f24d3f6106ef2c16380705be6885
yuguotianqing <believe.chenyu@gmail.com>
2016-06-25 15:05:10 +0800

    no change

--- src/ttree.h
+++ src/ttree.h
@@ -10,7 +10,7 @@ class TTreeNode
 public:
     TTreeNode();
     ~TTreeNode();
-    
+
     PlacedBox box;
     TTreeNode *l, *m, *r;
 };
@@ -20,32 +20,32 @@ class TTree
 public:
     TTree(const BoxList &boxes);
     ~TTree();
-    
+
     /// Delete the box p
     void Delete(int p);
-    
+
     /// Insert the box p as box q's child
     void InsertAsChild(int p, int q);
-    
+
     /// Insert the box p to box q's place and let box q be the k-th chlid of box p
     void InsertToReplace(int p, int q, int k);
-  
-      /// Swap 2 boxed at T-tree
+
+    /// Swap 2 boxed at T-tree
     void Swap(int p, int q);
-    
+
     /// Rotation box p around the axis at direction dir
     void Rotate(int p, int dir);
-    
+
     /// Get the minimum bounding box of the placement according to T-tree
     int GetVolume();
-    
+
     /// Get the optimal solution
     Solution GetSolution();
-    
+
 private:
     TTreeNode* root;
     TTreeNode* nodes;
-    
+
     /// Get T-tree node by box id
     TTreeNode* getNodeById(int id);
 };
